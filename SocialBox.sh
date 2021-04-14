e1af75b14a305e60542414eea5994db41bb614f2
david03489 <82496230+david03489@users.noreply.github.com>
2021-04-14 18:24:41 +0700

    sudo apt-get install git

--- SocialBox.sh
+++ SocialBox.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#Colors
-white="\033[1;37m"
-grey="\033[0;37m"
-purple="\033[0;35m"
-red="\033[1;31m"
-green="\033[1;32m"
-yellow="\033[1;33m"
-Purple="\033[0;35m"
-Cyan="\033[0;36m"
-Cafe="\033[0;33m"
-Fiuscha="\033[0;35m"
-blue="\033[1;34m"
-nc="\e[0m"
-#ROOT PRIVILEGIES
-if [[ $EUID -ne 0 ]]; then
+#Sysupdate
+sudo apt-get update 
+#Python
+sudo apt-get install python
+#gmail
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+#facebook
+if [ -d facebook ]; then
+        echo "Directory facebook exists.[✔]"
+else
+    mkdir facebook
+    cd facebook
+    wget raw.githubusercontent.com/Sup3r-Us3r/scripts/master/fb-brute.pl
+    cd ..
+fi
         echo -e "$red You don't have Root privilegies, execute the script as root.$nc"
         exit 1
 fi
